360d5135ae90e38636aa703f735240506c160422
Wagamama-xyz <68207830+Wagamama-xyz@users.noreply.github.com>
2023-09-09 18:36:36 +0700

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Sep 9, 2023',
+  birthdate: 'Sep 15, 2023',
   name: 'Orion'
 };
 
