2e569c943bd5081852e72244a01541d6581e7cdc
Wagamama-xyz <68207830+Wagamama-xyz@users.noreply.github.com>
2024-12-05 22:01:47 +0700

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Dec 22, 2024',
+  birthdate: 'Dec 05, 2024',
   name: 'Erina'
 };
 
